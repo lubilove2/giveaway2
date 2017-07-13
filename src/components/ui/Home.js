aa42878c79a47285af70a6b9645bbafab4867402
lubilove2 <lubaba_yemer@yahoo.com>
2017-07-13 10:05:23 -0400

    Add files via upload

---
+++ src/components/ui/Home.js
@@ -0,0 +1,10 @@
+import MainMenu from './MainMenu'
+
+const Home = () =>
+    <div className="home">
+        <MainMenu className="home-page-menu"/>
+        <div id="homebox">
+            <h1>Give Away</h1>
+        </div>
+    </div>
+export default Home
